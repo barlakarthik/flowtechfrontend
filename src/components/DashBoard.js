74a79e882d1a05fe2d55f708eb2d3f0177805176
barlakarthik <karthikyadav1727@gmail.com>
2023-10-16 19:44:00 +0530

    side bar transition

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,5 +1,6 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import './Dashboard.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const DashBoard = () => {
     const [sidebar, setSidebar] = useState(true);
@@ -12,11 +13,13 @@ const DashBoard = () => {
             <nav class="navbar navbar-expand-lg" style={{ backgroundColor: "rgb(48, 117, 184)" }}>
                 <span className='ms-2'><FontAwesomeIcon icon={faBars} style={{ color: "white" }} onClick={ToggleSideBar} /></span>
             </nav>
-            {sidebar && (
-                <aside style={{ width: "15%", backgroundColor: "rgb(45, 51, 84)", height: "100vh" }}>
+            <div className="dashboard-container" style={{display:"flex"}}>
 
+                <aside className={`sidebar ${sidebar ? 'open' : 'closed'}`}>
                 </aside>
-            )}
+                <main className="main-content">
+                </main>
+            </div>
         </div>
     )
 }
